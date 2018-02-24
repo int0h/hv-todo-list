1e879cfef466aa36e60bc03e55f02c28b7c62afa
int0h <kvv150794@yandex.ru>
2018-02-25 01:46:06 +0300

    editing & gitignore

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import {HyperValue} from 'hv';
-import {jsx, Component} from 'hv-jsx';
+import {jsx, Component, HvNode} from 'hv-jsx';
 import {renderIn} from 'hv-dom';
 
 const keyCodes = {
@@ -119,18 +119,59 @@ interface TodoItemProps {
 }
 
 class TodoItemView extends Component<TodoItemProps> {
+    editing = new HyperValue(false);
+    editText = new HyperValue(this.props.title.$);
+    editElem: HvNode;
+
+    className = this.hs.auto(() => {
+        const classes = [];
+
+        if (this.props.completed.$) {
+            classes.push('complited');
+        }
+
+        if (this.editing.$) {
+            classes.push('editing');
+        }
+
+        return classes.join(' ');
+    });
+
     toggle = () => {
         this.props.completed.$ = !this.props.completed.$;
     }
 
+    save = () => {
+        this.props.title.$ = this.editText.$;
+        this.toggleEditMode();
+    }
+
+    toggleEditMode = () => {
+        this.editing.$ = !this.editing.$;
+        this.editText.$ = this.props.title.$;
+        this.editElem.targetNodes[0].focus();
+    }
+
+    handleKey = (e: KeyboardEvent) => {
+        this.editText.$ = this.editElem.targetNodes[0].value;
+
+        switch (e.which) {
+            case keyCodes.escape:
+                this.editText.$ = this.props.title.$;
+            case keyCodes.enter:
+                this.editElem.targetNodes[0].blur();
+                break;
+        }
+    }
+
     render() {
-        return <li class={this.hs.auto(() => this.props.completed.$ ? 'completed' : '')} data-id="{{id}}">
+        return <li class={this.className}>
             <div class="view">
                 <input class="toggle" type="checkbox" checked={this.hs.auto(() => this.props.completed.$)} onChange={this.toggle}/>
-                <label>{this.props.title}</label>
+                <label onDblClick={this.toggleEditMode}>{this.props.title}</label>
                 <button class="destroy" onClick={() => this.props.onRemove()}></button>
             </div>
-            <input class="edit" value={this.props.title} />
+            <input class="edit" value={this.editText} onBlur={this.save} onKeyUp={this.handleKey} ref={elem => this.editElem = elem} />
         </li>;
     }
 }
